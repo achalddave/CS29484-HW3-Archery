8239d8c4f21922341815a82297c929ebb24dc50b
Achal Dave <achalddave@live.com>
2013-09-29 05:24:48 +0100

    svg.js targets/gradients/pretty

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,63 +1,142 @@
-SVG.Target = function(type) {
+/* 
+ * The SVG object extensions are admittedly overkill; I wanted to learn to play
+ * with svg.js
+ */
+SVG.Target = function(parent) {
   this.constructor.call(this, SVG.create('target'))
+  // from largest to smallest
+  this.rings = parent.doc().set();
+  this.stand = parent.rect(0,0).center(0,0).attr({fill : '#AD871C'});
+
+  var self = this;
+  this.colors.forEach(function(color) {
+    self.rings.add(parent.circle(0)
+                      .attr({fill : color, stroke : '#000'}));
+  });
+
+  var gradient = parent.gradient('radial', function(stop) {
+    stop.at({ offset: 0, color: '#eee', opacity: 0.8 })
+    stop.at({ offset: 1, color: '#000', opacity: 0.3 })
+  });
+  gradient.from(0.3,0.3).to(0.3,0.3).radius(0.3);
+  this.gradientRing = parent.circle(0).attr({fill : gradient});
 }
 
 // Inherit from SVG.Container
-SVG.Target.prototype = new SVG.Shape
+SVG.Target.prototype = new SVG.Shape();
 
-//
-SVG.extend(SVG.Container, {
-  target : function(x,y,r) {
-    // ring colors from outside in
-    var colors = ['#eee',
-                  'black',
-                  'blue',
-                  'red',
-                  'yellow','yellow'];
-
-    // radius of each ring
-    var step = r / colors.length;
-    var currRadius = r;
-    var svg = this;
-    colors.forEach(function(color) {
-      svg.circle(currRadius).center(x,y).attr({'fill' : color, 'stroke' : '#000'});
-      currRadius -= step;
-    });
-    // svg.circle(r).center(x,y).attr({'fill' : 'r(0.5, 0.5)rgba(0,0,0,255)-rgba(0,0,0,0)', 'opacity' : 0, 'opacityStops'});
-    // return svg.rect(50,50).attr({ fill : '#f06' });
+['cx','cy'].forEach(function(prop) {
+  SVG.Target.prototype[prop] = function(val) {
+    if (val == null) {
+      return this.gradientRing[prop]();
+    } else {
+      this.rings[prop](val);
+      this.gradientRing[prop](val);
+      this.repositionStand();
+      return this;
+    }
   }
 });
 
-var debugPaper = true;
+['x','y'].forEach(function(prop) {
+  SVG.Target.prototype[prop] = function(val) {
+    var rProp = 'r' + prop, cProp = 'c' + prop;
+    this[cProp](val + this.gradientRing.attr(rProp));
+    return this;
+  }
+});
+
+SVG.extend(SVG.Target, {
+  // from largest to smallest
+  colors: ['#eee', 'black', 'blue', 'red', 'yellow', 'yellow'],
+  repositionStand: function() {
+    this.stand.cx(this.gradientRing.cx());
+    // not a typo, align top with center
+    this.stand.y(this.gradientRing.cy());
+  },
+  size: function(r) {
+    var currDiameter = 2*r;
+    var step = 2*r / this.colors.length;
+
+    this.stand.size(.5*r, 1.3*r);
+    this.gradientRing.size(2*r, 2*r);
+    this.repositionStand();
+
+    this.rings.each(function() {
+      // `this` refers to each ring
+      this.size(currDiameter, currDiameter);
+      currDiameter -= step;
+    });
+    return this;
+  },
+  placeGround: function(cx, by) {
+    return this.cx(cx).y(by - this.stand.attr('height') - this.gradientRing.attr('ry'));
+  }
+});
+
+SVG.extend(SVG.Container, {
+  target : function(r) {
+    // ring colors from outside in
+    return this.put(new SVG.Target(this)).size(r).move(0,0);
+  }
+});
+
+// config
+var config = {
+  numSlots : 9,
+  debug : true
+}
 $(function() {
   var svg = SVG('game');
   var w = $('#game').width(), h = $('#game').height();
-  if (debugPaper) window.svg = svg;
-  svg.target(20,20,50);
-  // svg.rect(50,50).center(w/2, h/2).attr({ fill: '#f06' });
-});
+  // line at which target can be placed
+  var skyGradient = svg.gradient('linear', function(stop) {
+    stop.at({ offset: 0, color: '#888FF7', opacity: 0.8 })
+    stop.at({ offset: 1, color: 'darkblue', opacity: 0.8 })
+  }).from(0.5,0).to(0.5,1);
+  var grassGradient = svg.gradient('linear', function(stop) {
+    stop.at({ offset: 0, color: '#18A300', opacity: 0.8 })
+    stop.at({ offset: 1, color: '#28DB09', opacity: 0.8 })
+  }).from(0.5,0).to(0.5,1);
+  // var grass = svg.rect(w,h*0.7).y(0.3*h).attr({'fill' : '#84FF3D'});
+  // var sky = svg.rect(w,h*0.3).attr({'fill' : '#888FF7'});
+  var grass = svg.rect(w,h*0.7).y(0.3*h).attr({fill : grassGradient});
+  var sky = svg.rect(w,h*0.3).attr({fill : skyGradient});
+  var targetGround = svg.line(0,0.7*h,w,0.7*h)
+  if (config.debug) targetGround.stroke({ width: 1 });
 
-/*
-Raphael.fn.target = function(x,y,r) {
-  // ring colors from outside in
-  var colors = ['#eee',
-                'black',
-                'blue',
-                'red',
-                'yellow','yellow'];
+  var slots = new Array(config.numSlots);
+  function Slot(index) { 
+    this.center = (index + 0.5)*this.width;
+    this.rightLine = null; 
+  }
+  Slot.prototype.bottom = targetGround.attr('y1');
+  Slot.prototype.width = w / slots.length;
 
-  // radius of each ring
-  var step = r / colors.length;
-  var currRadius = r;
-  var paper = this;
-  colors.forEach(function(color) {
-    paper.circle(x,y,currRadius).attr({'fill' : color, 'stroke' : '#000'});
-    currRadius -= step;
+  for (var i = 0; i < config.numSlots; ++i) {
+    var slot = new Slot(i);
+    var rightLineX = (i+1) * slot.width;
+    slot.rightLine = svg.line(rightLineX, 0, rightLineX, h);
+    if (config.debug) slot.rightLine.stroke({width:1});
+    slots[i] = slot;
+  }
+
+  function updateSlot(i) {
+    target.placeGround(slots[i].center, slots[i].bottom);
+    arrow.attr({
+      x1 : slots[i].center,
+      x2 : slots[i].center
+    });
+  }
+
+  var defaultSlot = Math.round(slots.length/2-0.5);
+  var target = svg.target(40);
+  var arrow  = svg.line(0,h,0,h-(h - slots[0].bottom)/2).stroke({width:5});
+  updateSlot(defaultSlot);
+
+  $('#target-pos').attr({ min : 0, max : slots.length, step: 1 });
+  $('#target-pos').change(function() {
+    updateSlot($(this).val());
   });
-  paper.circle(x,y,r).attr({'fill' : 'r(0.5, 0.5)rgba(0,0,0,255)-rgba(0,0,0,0)', 'opacity' : 0, 'opacityStops'});
-}
 
-Raphael.fn.bow = function(x,y,h) {
-  
-}
-*/
+});
